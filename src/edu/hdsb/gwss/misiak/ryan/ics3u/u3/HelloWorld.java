f8cf932fe7117ae04f4f09e634122dff46568ce8
1misiakrya <1misiakrya@gws400011.ihdsb.ca>
2015-02-24 10:55:01 -0500

    - initialize ICS3U-2014-15.S2

---
+++ src/edu/hdsb/gwss/misiak/ryan/ics3u/u3/HelloWorld.java
@@ -0,0 +1,26 @@
+/*
+ * Name: HelloWorld.java
+ * Date: Feb 24th, 2015
+ * Version: v0.1
+ * Author: Mr. R. Misiak
+ * Description: Simple program to display "hello world" to the screen.
+*/
+
+package edu.hdsb.gwss.misiak.ryan.ics3u.u3;
+
+/**
+ *
+ * @author 1misiakrya
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    
+        System.out.println("Hello World");
+        System.err.println("Hello World");
+    }
+    
+}
