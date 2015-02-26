eb0f1a360f719c8333ad95371776da417799bb6f
1misiakrya <1misiakrya@gws33866.ihdsb.ca>
2015-02-26 11:11:56 -0500

    - Created the following small assignments for the date February 26, 2015.

---
+++ src/edu/hdsb/gwss/misiak/ryan/ics3u/u3/DoubleJeopardy.java
@@ -0,0 +1,18 @@
+/*
+ * Name: DoubleJeopardy
+ * Date: February 26, 2015
+ * Version: v0.1
+ * Author: Mr. R. Misiak
+ * Descrption: This program was designed to test the limits of a double
+(exponent value of 309).
+ */
+package edu.hdsb.gwss.misiak.ryan.ics3u.u3;
+
+public class DoubleJeopardy {
+
+    public static void main(String[] args) {
+        double value = 32e309;
+        System.out.println("A double: " +  value);
+    }
+    
+}
---
+++ src/edu/hdsb/gwss/misiak/ryan/ics3u/u3/Shortfall.java
@@ -0,0 +1,21 @@
+/*
+ * Name: DoubleJeopardy
+ * Date: February 26, 2015
+ * Version: v0.1
+ * Author: Mr. R. Misiak
+ * Descrption:
+ */
+package edu.hdsb.gwss.misiak.ryan.ics3u.u3;
+
+/**
+ *
+ * @author 1misiakrya
+ */
+public class Shortfall {
+
+    public static void main(String[] args) {
+    int value = 35000;
+    System.out.println("A short: " + value);
+    }
+    
+}
