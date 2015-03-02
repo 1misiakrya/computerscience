b1db47a43a4c4a87649a552d7372844efeb1e293
1misiakrya <1misiakrya@gws400771.ihdsb.ca>
2015-03-02 09:44:19 -0500

    - Created Digits, Perimeter and HeronFormula, and worked on Arithmetic, Change and Restaurant.

--- src/edu/hdsb/gwss/misiak/ryan/ics3u/u3/HeronFormula.java
+++ src/edu/hdsb/gwss/misiak/ryan/ics3u/u3/HeronFormula.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Name: HeronFormula
+ * Date: March 2, 2015
+ * Version: v0.1
+ * Author: Mr. R. Misiak
+ * Descrption: This program gives the area of a triangle using Heron's Formula, based off of the user's input.
  */
 package edu.hdsb.gwss.misiak.ryan.ics3u.u3;
 
