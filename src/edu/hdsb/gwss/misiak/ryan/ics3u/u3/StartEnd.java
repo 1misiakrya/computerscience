854a76e5004728c28bf20213d653c59ae6d0d57d
1misiakrya <1misiakrya@gws400021.ihdsb.ca>
2015-03-30 11:09:56 -0400

    - Created StartEnd

---
+++ src/edu/hdsb/gwss/misiak/ryan/ics3u/u3/StartEnd.java
@@ -0,0 +1,24 @@
+/*
+ * Name: StartEnd
+ * Date: March 30, 2015
+ * Version: v0.1
+ * Author: Mr. R. Misiak
+ * Description: This program asks the user for a starting value and an ending value, and then writes all the integers (inclusive) between those two values.
+ */
+package edu.hdsb.gwss.misiak.ryan.ics3u.u3;
+
+/**
+ *
+ * @author 1misiakrya
+ */
+public class StartEnd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
