c047d49ea902ef86370b9b6b55d5886f2ae12b08
RMisiak <RMisiak@192.168.10.2>
2015-04-01 15:56:07 -0400

    - Created CloserToTwo and finished CompoundInvesting.

---
+++ src/edu/hdsb/gwss/misiak/ryan/ics3u/u4/CloserToTwo.java
@@ -0,0 +1,24 @@
+/*
+ * Name: CloserToTwo
+ * Date: April 1, 2015
+ * Version: v0.1
+ * Author: Mr. R. Misiak
+ * Description: This program demonstrates that the result of adding the numbers 1, 1/2, 1/4, 1/16 ...
+and so on gets closer to 2 without ever becoming 2.
+ */
+package edu.hdsb.gwss.misiak.ryan.ics3u.u4;
+
+/**
+ *
+ * @author RMisiak
+ */
+public class CloserToTwo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
--- src/edu/hdsb/gwss/misiak/ryan/ics3u/u4/CompoundInvesting.java
+++ src/edu/hdsb/gwss/misiak/ryan/ics3u/u4/CompoundInvesting.java
@@ -9,6 +9,7 @@
 package edu.hdsb.gwss.misiak.ryan.ics3u.u4;
 
 import java.text.NumberFormat;
+import java.util.Scanner;
 
 /**
  *
@@ -21,21 +22,45 @@ public class CompoundInvesting {
      */
     public static void main(String[] args) {
 
-        double initialInvestment = 150.00;
-        double interestRate = 0.09;
-        double accountTotal = initialInvestment;
+        //Explanations
+        System.out.println("Compound Investing");
+        System.out.println("This program will print out a title table that will display"
+                + "the amount of a yearly investment over a period of up to 15 years.");
+
+        //Getting the user's input
+        System.out.format("%20s %1s", "Enter the yearly investment", " : ");
+
+        Scanner inputYearlyInvestment = new Scanner(System.in);
+        double yearlyInvestment = inputYearlyInvestment.nextDouble();
+
+        System.out.format("%20s %1s", "Enter the interest rate (%)", " : ");
+
+        Scanner inputInterestRate = new Scanner(System.in);
+        double interestRate = inputInterestRate.nextDouble();
+
+        System.out.format("%20s %1s", "Enter the number of years", " : ");
+
+        Scanner inputNumberOfYears = new Scanner(System.in);
+        int years = inputNumberOfYears.nextInt();
+
+        //Other totals needed
+        double accountTotal = yearlyInvestment;
         double totalInterest;
         double finalTotal;
 
-        int years = 10;
-        
+        //Formatting currency
         NumberFormat money = NumberFormat.getCurrencyInstance();
-        
-        for (int year = 1; year <= years; year += 1) {
-            totalInterest = accountTotal*interestRate;
-            finalTotal = totalInterest + accountTotal;
-            System.out.format("%5s %10s %10s %10s\n", year, money.format(accountTotal), money.format(totalInterest), money.format(accountTotal+(accountTotal*interestRate)));
-            accountTotal = accountTotal + finalTotal;
+
+        //OUTPUT
+        if (years > 15 || years < 0) {
+            System.out.println("Error: INVALID NUMBER OF YEARS. Must be above 0 and less than or equal to 15.");
+        } else {
+            for (int year = 1; year <= years; year += 1) {
+                totalInterest = accountTotal * interestRate;
+                finalTotal = totalInterest + accountTotal;
+                System.out.format("%5s %15s %15s %15s\n", year, money.format(accountTotal), money.format(totalInterest), money.format(accountTotal + (accountTotal * interestRate)));
+                accountTotal = yearlyInvestment + finalTotal;
+            }
         }
 
     }
