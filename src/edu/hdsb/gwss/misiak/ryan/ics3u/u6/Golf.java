48cf22216cc1984fe6248c822277b06d97f19efc
1misiakrya <1misiakrya@gws400061.ihdsb.ca>
2015-04-30 11:14:59 -0400

    - Created GolfStats GUI and finished regular Golf program.

--- src/edu/hdsb/gwss/misiak/ryan/ics3u/u6/Golf.java
+++ src/edu/hdsb/gwss/misiak/ryan/ics3u/u6/Golf.java
@@ -1,9 +1,13 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Name: Golf
+ * Date: April 27, 2015
+ * Version: v0.1
+ * Author: Mr. R. Misiak
+ * Description: This program records the score for 18 holes of golf.
  */
 package edu.hdsb.gwss.misiak.ryan.ics3u.u6;
+
+import java.util.Scanner;
 
 /**
  *
@@ -15,7 +19,20 @@ public class Golf {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        System.out.println("This program records the score for 18 holes of golf.");
+
+        Scanner inputScore = new Scanner(System.in);
+        int[] userScore = new int[18];
+
+        for (int hole = 0; hole < 18; hole++) {
+            System.out.print("Please enter your score in hole " + (hole + 1) + ": ");
+            userScore[hole] = inputScore.nextInt();
+        }
+        for (int hole = 0; hole < 18; hole++) {
+            System.out.println("Golf hole " + (hole + 1) + ";" + userScore[hole]);
+        }
+
     }
-    
+
 }
---
+++ src/edu/hdsb/gwss/misiak/ryan/ics3u/u6/Temperature.java
@@ -0,0 +1,23 @@
+/*
+ * Name: Temperature
+ * Date: April 30, 2015
+ * Version: v0.1
+ * Author: Mr. R. Misiak
+ * Description: This program 
+ */
+package edu.hdsb.gwss.misiak.ryan.ics3u.u6;
+
+/**
+ *
+ * @author 1misiakrya
+ */
+public class Temperature {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
