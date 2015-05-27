4d875d4ac56b0b80bb49f8d7b7375743d50db5e4
1misiakrya <1misiakrya@gws400081.ihdsb.ca>
2015-05-27 11:08:47 -0400

    - Started and finished AnimalSearch, and started SoccerTeams for the assignment.

---
+++ src/edu/hdsb/gwss/misiak/ryan/ics3u/u7/AnimalSearch.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.hdsb.gwss.misiak.ryan.ics3u.u7;
+
+import java.io.File;
+import nu.xom.Builder;
+import nu.xom.Document;
+import nu.xom.Element;
+import nu.xom.Elements;
+
+/**
+ *
+ * @author 1misiakrya
+ */
+public class AnimalSearch {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        File file = new File("animals.xml");
+
+        Builder builder = new Builder();
+        Document animalsDocument;
+        Element animalsRoot;
+
+        try {
+            animalsDocument = builder.build(file);
+            animalsRoot = animalsDocument.getRootElement();
+            Elements animals = animalsRoot.getChildElements();
+            for (int i = 0; i < animals.size(); i++) {
+                if (animals.get(i).getFirstChildElement("animalClass").getValue().equals("Aves")) {
+                    System.out.println(animals.get(i).getFirstChildElement("animalName").getValue());
+                }
+            }
+            for (int i = 0; i < animals.size(); i++) {
+                if (animals.get(i).getFirstChildElement("diet").getValue().equals("Carnivore")) {
+                    System.out.println(animals.get(i).getFirstChildElement("animalName").getValue());
+                }
+            }
+            for (int i = 0; i < animals.size(); i++) {
+                if (animals.get(i).getFirstChildElement("habitat").getValue().equals("North America")) {
+                    System.out.println(animals.get(i).getFirstChildElement("animalName").getValue());
+                }
+            }
+            for (int i = 0; i < animals.size(); i++) {
+                if (animals.get(i).getFirstChildElement("animalName").getValue().equals("Moose")) {
+                    System.out.println(animals.get(i).getFirstChildElement("animalName").getValue());
+                }
+            }
+            for (int i = 0; i < animals.size(); i++) {
+                if (animals.get(i).getFirstChildElement("animalName").getValue().equals("Toucan")) {
+                    System.out.println("Toucan");
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
